5f9900df7d6b72faefdfcba1591fe816fa3c0f7b
Your Name <jaceman44@gmail.com>
2022-05-01 22:06:06 +0200

    Added baisic version of custom algorithm

--- coalition/dutch_auction.py
+++ coalition/dutch_auction.py
@@ -13,24 +13,36 @@ class RobotCost():
 
     # TODO: Temporary energy coefficients hardcoded; fix this!
 
-    COF_ENE = 2
-    COF_LOADED = 1
+    COF_ENE = 0.05
+    COF_LOADED = 0.025
 
     robot_energy_cost: float
+
+    ''' 
+    Weighted cost of a robot; 
+    
+    robot_cost * [WEIGHT_1] + robot_energy_cost * [WEIGHT_2]'''
+    #robot_weighted_cost: float
 
     def __init__(self, robot: Robot, initial_cost, epsilon):
         self.robot = robot
         self.cost = initial_cost + epsilon
+        self.robot_weighted_cost = 0
 
     def update_cost(self) -> None:
         pass
 
-    ''' Calculate  '''
+    ''' Calculate  energy cost'''
+
     def energy_cost(self, box: Box) -> None:
         x_target, y_target = box.target
         self.robot_energy_cost = self.robot.get_distance_to(box.x, box.y)*self.COF_ENE + \
             box.get_distance_to(x_target, y_target) * \
             (self.COF_ENE+box.points_of_support*self.COF_LOADED)
+
+    def weighted_robot_cost(self, weight: List[float, float]) -> None:
+        self.robot_weighted_cost = self.cost * \
+            weight[0] + self.robot_energy_cost * weight[1]
 
 
 class DutchAuction:
@@ -42,7 +54,7 @@ class DutchAuction:
     '''epsilon - auction's constant
     '''
 
-    def __init__(self, robots: List[Robot], boxes: Set[Box], epsilon=1.0, robots_capacity=100):
+    def __init__(self, robots: List[Robot], boxes: Set[Box], cost_weights: List[float, float], epsilon=1.0, robots_capacity=100):
         self.robots = robots
         self.epsilon = epsilon
         self.robots_capacity = robots_capacity
@@ -50,55 +62,69 @@ class DutchAuction:
         self.profitability_box_list = sorted(
             boxes, key=lambda box: box.profitability, reverse=True)
 
+        self.cost_weights = cost_weights
+
     def start_auction(self):
         ''' Remove robot whose battery level is below or equal critical'''
-        self.discar_flat_robots()
+        self._discar_flat_robots()
+
+        copy_robots_cost = self.robots_costs
 
         for box_task in self.profitability_box_list:
             ''' Calculate every robot's cost '''
-            self.create_robots_queue_list(
-                box_task.profitability)
+            self.robots_costs = [RobotCost(
+                robot, box_task.profitability, self.epsilon) for robot in self.robots]
 
             ''' Calculate how many robots are needed to move the box '''
             # self.calculate_robot_needed()
 
             # while(True):
             ''' Lower the cost of the most expensive robot '''
-            self.change_robot_cost()
+            #self._change_robot_cost()
 
-            ''' Create precoalition and check if precoalition is suitable for the box/task'''
-            pre_coalition = self.create_pre_coalition(box_task)
-            pass
+            while True:
+                ''' Create precoalition and check if precoalition is suitable for the box/task'''
+                pre_coalition = self._create_pre_coalition(box_task)
 
-    def discar_flat_robots(self) -> None:
+                if pre_coalition is None:
+                    self._change_robot_cost(self.cost_weights)
+                else:
+                    robot_temp_class = [wrapper_class.robot for wrapper_class in pre_coalition]
+                    return robot_temp_class, box_task
+                    #break
+
+    def _discar_flat_robots(self) -> None:
         for robot in self.robots:
             if robot.battery_level <= robot.critical_battery_level:
                 self.robots.remove(robot)
 
-    def create_robots_queue_list(self, profitability: float) -> None:
-        for robot in self.robots:
-            self.robots_costs.append(
-                RobotCost(robot, profitability, self.epsilon))
-
-    def calculate_robot_needed(self) -> None:
+    def _calculate_robot_needed(self) -> None:
         """ Calculate how many robots are needed to complete given task (push particular box to the desired destination) """
         for box in self.profitability_box_list:
             box.num_of_robot_needed = int(box.mass/self.robots_capacity) + \
                 1 if box.mass % self.robots_capacity != 0 else 0
 
-    def change_robot_cost(self) -> None:
+    def _change_robot_cost(self, weights) -> None:
+        for robot in self.robots_costs:
+            robot.weighted_robot_cost(weights)
+
         self.robots_costs = sorted(
-            self.robots_costs, key=lambda robot: robot.cost, reverse=True)
+            self.robots_costs, key=lambda robot: robot.robot_weighted_cost, reverse=True)
 
         self.robots_costs[0].cost = self.robots_costs[0].cost-self.epsilon
 
-    def create_pre_coalition(self, box: Box) -> List:
+        if self.robots_costs[0].cost == 0:
+            self.robots_costs.remove(self.robots_costs[0])
+
+
+    def _create_pre_coalition(self, box: Box) -> List:
         if len(self.robots) > box.points_of_support:
+
             for robot in self.robots_costs:
                 robot.energy_cost(box)
 
             energy_cost_list = sorted(
-                self.robots_costs, key=lambda robot_energy_c: robot_energy_c.robot_energy_cost)
+                self.robots_costs, key=lambda robot_energy_c: robot_energy_c.robot_weighted_cost)
 
             pre_coalition = energy_cost_list[0:box.points_of_support]
 
@@ -108,6 +134,7 @@ class DutchAuction:
 
             if box.profitability >= pre_coalition_cost:
                 # TODO: Add robots removing
+                print(pre_coalition)
                 return pre_coalition
 
             return None
--- main.py
+++ main.py
@@ -13,20 +13,21 @@ w1.generate_random_boxes(10)
 w1.generate_random_robots(14)
 r1 = list(w1.robots)[0:2]
 box1 = list(w1.boxes_left)[0]
-r2 = list(w1.robots)[3:5]
-box2 = list(w1.boxes_left)[1]
+#r2 = list(w1.robots)[3:5]
+#box2 = list(w1.boxes_left)[1]
 
 test_robot = list(w1.robots)[0]
-test_robot.battery_level=0
+test_robot.battery_level = 0
 
 list(w1.robots)[0] = test_robot
 
-coalition = DutchAuction(w1.robots, w1.boxes_left)
+cost_weights = [0.3, 0.7]
 
-coalition.start_auction()
+coalition = DutchAuction(w1.robots, w1.boxes_left, cost_weights=cost_weights)
 
+col, box = coalition.start_auction()
 
-coal1 = {RobotCoalition(set(r1), box1), RobotCoalition(set(r2), box2)}
+coal1 = {RobotCoalition(set(col), box)}
 w1.coalitions = coal1
 
 while len(w1.boxes_done) == 0:
